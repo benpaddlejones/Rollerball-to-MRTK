f195e8647632f44bb5b3a26a236f4ad4204f77f0
Ben Jones <benpaddlejones@outlook.com>
2019-08-22 13:22:52 +1000

    initial commit

---
+++ Assets/Scripts/camPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
